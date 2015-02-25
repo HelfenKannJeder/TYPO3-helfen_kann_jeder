387e42e1e55454183832e74ffdd53a25f4e6680e
Valentin Zickner <zickner@querformatik.de>
2015-02-25 15:13:11 +0100

    Cleaned code of DraftToLiveService.

--- Classes/Service/DraftToLiveService.php
+++ Classes/Service/DraftToLiveService.php
@@ -3,14 +3,20 @@ namespace Querformatik\HelfenKannJeder\Service;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-
+/**
+ * This class is to publish an organisation. For this case, it copies all
+ * Draft-Objects to the Live-Objects.
+ *
+ * @author Valentin Zickner
+ */
 class DraftToLiveService implements \TYPO3\CMS\Core\SingletonInterface {
 
 	/**
-	 * @var \Querformatik\HelfenKannJeder\Domain\Repository\OrganisationDraftRepository
+	 * @var
+	 * 	\Querformatik\HelfenKannJeder\Domain\Repository\OrganisationDraftRepository
 	 * @inject
 	 */
-	protected $organisationDraftRepository;
+	protected $draftRepository;
 
 	/**
 	 * @var \TYPO3\CMS\Extbase\Persistence\Generic\PersistenceManager
@@ -21,13 +27,16 @@ class DraftToLiveService implements \TYPO3\CMS\Core\SingletonInterface {
 	/**
 	 * Copy an OrganisationDraft object to the corresponding Organisation object.
 	 *
-	 * @param \Querformatik\HelfenKannJeder\Domain\Model\OrganisationDraft $organisationDraft
+	 * @param \Querformatik\HelfenKannJeder\Domain\Model\OrganisationDraft
+	 * 			$organisationDraft
+	 * @return void
 	 */
 	public function draft2live(\Querformatik\HelfenKannJeder\Domain\Model\OrganisationDraft $organisationDraft) {
 		$organisationDraft->setRequest(2);
 		$organisationDraft->setRequesttime(time());
-		$destObject = $this->syncObject("\\Querformatik\\HelfenKannJeder\\Domain\\Model\\OrganisationDraft", "\\Querformatik\\HelfenKannJeder\\Domain\\Model\\Organisation", $organisationDraft);
-		$this->organisationDraftRepository->update($organisationDraft);
+		$this->syncObject('\\Querformatik\\HelfenKannJeder\\Domain\\Model\\OrganisationDraft',
+			'\\Querformatik\\HelfenKannJeder\\Domain\\Model\\Organisation', $organisationDraft);
+		$this->draftRepository->update($organisationDraft);
 
 		$this->persistenceManager->persistAll();
 	}
@@ -35,7 +44,7 @@ class DraftToLiveService implements \TYPO3\CMS\Core\SingletonInterface {
 	/**
 	 * Syncs one object to an equal object.
 	 */
-	private function syncObject($srcClass, $destClass, &$srcObject, $methodsWalkedThrough=array()) {
+	private function syncObject($srcClass, $destClass, &$srcObject, $methodsWalkedThrough = array()) {
 		// Prove the parameters (srcClass is correct)
 		if ($srcObject instanceof $srcClass) {
 			// Use old reference or create new one.
@@ -47,7 +56,8 @@ class DraftToLiveService implements \TYPO3\CMS\Core\SingletonInterface {
 
 			$methodsWalkedThrough[] = $srcObject->__toString();
 
-			if (!($destObject instanceof $destClass) && !($destObject instanceof \TYPO3\CMS\Extbase\Persistence\Generic\LazyLoadingProxy && $destObject->getUid() != 0)) {
+			if (!($destObject instanceof $destClass) &&
+				!($destObject instanceof \TYPO3\CMS\Extbase\Persistence\Generic\LazyLoadingProxy && $destObject->getUid() != 0)) {
 				$destObject = new $destClass();
 				$destObject->setReference($srcObject);
 				$srcObject->setReference($destObject);
@@ -64,11 +74,12 @@ class DraftToLiveService implements \TYPO3\CMS\Core\SingletonInterface {
 			$classSchema = $reflectionService->getClassSchema($srcClass);
 
 			foreach ($srcMethods as $srcMethod) {
-				if (substr($srcMethod,0,3) == "get" && $srcMethod != "getReference" && $srcMethod != "getUid" && $srcMethod != "getPid") { // Only use getter methods for finding arrays
-					$srcMethodAttribute = lcfirst(substr($srcMethod,3));
-					$destMethodSet = "set" . substr($srcMethod,3);
-					$destMethodAdd = "add" . substr($srcMethod,3);
-					$destMethodRemove = "remove" . substr($srcMethod,3);
+				// Only use getter methods for finding arrays
+				if (substr($srcMethod, 0, 3) == 'get' && $srcMethod != 'getReference' && $srcMethod != 'getUid' && $srcMethod != 'getPid') {
+					$srcMethodAttribute = lcfirst(substr($srcMethod, 3));
+					$destMethodSet = 'set' . substr($srcMethod, 3);
+					$destMethodAdd = 'add' . substr($srcMethod, 3);
+					$destMethodRemove = 'remove' . substr($srcMethod, 3);
 					$columnMap = $dataMap->getColumnMap($srcMethodAttribute);
 					$propertyData = $classSchema->getProperty($srcMethodAttribute);
 
@@ -76,21 +87,23 @@ class DraftToLiveService implements \TYPO3\CMS\Core\SingletonInterface {
 						$getterResult = call_user_func(array(&$srcObject, $srcMethod));
 						switch ($columnMap->getTypeOfRelation()) {
 							case \TYPO3\CMS\Extbase\Persistence\Generic\Mapper\ColumnMap::RELATION_HAS_MANY:
+								// Fallthrough, this is the same case
 							case \TYPO3\CMS\Extbase\Persistence\Generic\Mapper\ColumnMap::RELATION_HAS_AND_BELONGS_TO_MANY:
 								$destMethodAdd = substr($destMethodAdd, 0, -1);
 								if (!in_array($destMethodAdd, $destMethods)) {
 									$destMethodAdd = substr($destMethodAdd, 0, -1);
 								}
-								if (in_array($destMethodAdd, $destMethods) && in_array($srcMethod, $destMethods) && isset($propertyData["elementType"])) {
-									$newName = $this->buildNewClassName($srcClass, $destClass, $propertyData["elementType"]);
+								if (in_array($destMethodAdd, $destMethods) && in_array($srcMethod, $destMethods) && isset($propertyData['elementType'])) {
+									$newName = $this->buildNewClassName($srcClass, $destClass, $propertyData['elementType']);
 									if (class_exists($newName)) {
 										foreach ($getterResult as $getterResultEntry) {
-											$newCreatedObject = $this->syncObject($propertyData["elementType"], $newName, $getterResultEntry, $methodsWalkedThrough);
-											$alreadyAdded = false;
-											$getterResultNewObject = call_user_func(array(&$destObject, $srcMethod));
-											if ($newCreatedObject->__toString() != $newName.":") {
-												foreach ($getterResultNewObject as $getterResultNewObjectEntry) {
-													$alreadyAdded |= ($getterResultNewObjectEntry->__toString() == $newCreatedObject->__toString());
+											$newCreatedObject = $this->syncObject($propertyData['elementType'], $newName, $getterResultEntry,
+												$methodsWalkedThrough);
+											$alreadyAdded = FALSE;
+											$newObject = call_user_func(array(&$destObject, $srcMethod));
+											if ($newCreatedObject->__toString() != $newName . ':') {
+												foreach ($newObject as $newObjectEntry) {
+													$alreadyAdded |= ($newObjectEntry->__toString() == $newCreatedObject->__toString());
 												}
 											}
 
@@ -101,8 +114,6 @@ class DraftToLiveService implements \TYPO3\CMS\Core\SingletonInterface {
 												call_user_func(array(&$destObject, $destMethodAdd), $newCreatedObject);
 											}
 										}
-
-
 									} else {
 										foreach ($getterResult as $getterResultEntry) {
 											call_user_func(array(&$destObject, $destMethodAdd), $getterResultEntry);
@@ -120,15 +131,15 @@ class DraftToLiveService implements \TYPO3\CMS\Core\SingletonInterface {
 										}
 									}
 									if (in_array($destMethodRemove, $destMethods)) {
-										$getterResultNewObject = call_user_func(array(&$destObject, $srcMethod));
-										foreach ($getterResultNewObject as $getterResultNewObjectEntry) {
-											$ref = $getterResultNewObjectEntry->getReference();
+										$newObject = call_user_func(array(&$destObject, $srcMethod));
+										foreach ($newObject as $newObjectEntry) {
+											$ref = $newObjectEntry->getReference();
 											$refOrg = $ref;
 											if ($ref instanceof \TYPO3\CMS\Extbase\Persistence\Generic\LazyLoadingProxy) {
 												$ref = $ref->_loadRealInstance();
 											}
-											if (!($ref instanceof $propertyData["elementType"]) || !in_array($refOrg->__toString(), $getterResultStrings)) {
-												call_user_func(array(&$destObject, $destMethodRemove), $getterResultNewObjectEntry);
+											if (!($ref instanceof $propertyData['elementType']) || !in_array($refOrg->__toString(), $getterResultStrings)) {
+												call_user_func(array(&$destObject, $destMethodRemove), $newObjectEntry);
 											}
 										}
 									}
@@ -136,9 +147,9 @@ class DraftToLiveService implements \TYPO3\CMS\Core\SingletonInterface {
 								break;
 							case \TYPO3\CMS\Extbase\Persistence\Generic\Mapper\ColumnMap::RELATION_HAS_ONE:
 								if (in_array($destMethodSet, $destMethods)) {
-									$newName = $this->buildNewClassName($srcClass, $destClass, $propertyData["type"]);
+									$newName = $this->buildNewClassName($srcClass, $destClass, $propertyData['type']);
 									if (class_exists($newName)) {
-										$newCreatedObject = $this->syncObject($propertyData["type"], $newName, $getterResult, $methodsWalkedThrough);
+										$newCreatedObject = $this->syncObject($propertyData['type'], $newName, $getterResult, $methodsWalkedThrough);
 										call_user_func(array(&$destObject, $destMethodSet), $newCreatedObject);
 									} else {
 										call_user_func(array(&$destObject, $destMethodSet), $getterResult);
@@ -152,6 +163,8 @@ class DraftToLiveService implements \TYPO3\CMS\Core\SingletonInterface {
 									call_user_func(array(&$destObject, $destMethodSet), $getterResult);
 								}
 								break;
+							default:
+								throw new \Exception('System error, missing ColumnMap.');
 						}
 					}
 				}
@@ -164,10 +177,9 @@ class DraftToLiveService implements \TYPO3\CMS\Core\SingletonInterface {
 	private function buildNewClassName($oldClassName, $newClassName, $buildableClassName) {
 		if (strlen($oldClassName) < strlen($newClassName)) {
 			$buildableClassName .= substr($newClassName, strlen($oldClassName));
-		} else if (strlen($oldClassName) > strlen($newClassName)) {
-			$buildableClassName = substr($buildableClassName, 0, strlen($newClassName)-strlen($oldClassName));
+		} elseif (strlen($oldClassName) > strlen($newClassName)) {
+			$buildableClassName = substr($buildableClassName, 0, strlen($newClassName) - strlen($oldClassName));
 		}
 		return $buildableClassName;
 	}
 }
-?>
