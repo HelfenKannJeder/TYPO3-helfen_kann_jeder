156c0b3635b94539e7a9446d8c6ca71afe81b670
Valentin Zickner <zickner@querformatik.de>
2015-02-25 16:19:28 +0100

    Integrated complete code coverage support to test.sh.

--- Tests/Build/test.sh
+++ Tests/Build/test.sh
@@ -7,10 +7,12 @@ cd ../../../../../
 # Clean up
 rm -rf typo3temp/functional-*
 
+TEMPFOLDER=$(mktemp -d)
+
 echo;
 echo "Running unit tests";
 if [[ "$COVERAGE" == "1" ]]; then
-	phpunit --coverage-clover=coverage.clover --colors -c typo3conf/ext/helfen_kann_jeder/Tests/Build/UnitTests.xml
+	phpunit --coverage-php="$TEMPFOLDER/unit.cov" --colors -c typo3conf/ext/helfen_kann_jeder/Tests/Build/UnitTests.xml
 else
 	phpunit --colors -c typo3conf/ext/helfen_kann_jeder/Tests/Build/UnitTests.xml
 fi
@@ -18,7 +20,12 @@ fi
 echo;
 echo "Running functional tests";
 if [[ "$COVERAGE" == "1" ]]; then
-	find . -wholename '*/typo3conf/ext/helfen_kann_jeder/Tests/Functional/*Test.php' | parallel --gnu 'echo; echo "Running functional test suite {}"; phpunit --coverage-clover={}functionaltest-coverage.clover --colors -c typo3conf/ext/helfen_kann_jeder/Tests/Build/FunctionalTests.xml {}'
+	find . -wholename '*/typo3conf/ext/helfen_kann_jeder/Tests/Functional/*Test.php' | parallel --gnu 'echo; echo "Running functional test suite {}"; phpunit --coverage-php=$TEMPFOLDER/$(echo {} |md5sum -|head -c 32).cov --colors -c typo3conf/ext/helfen_kann_jeder/Tests/Build/FunctionalTests.xml {}'
 else
 	find . -wholename '*/typo3conf/ext/helfen_kann_jeder/Tests/Functional/*Test.php' | parallel --gnu 'echo; echo "Running functional test suite {}"; phpunit --colors -c typo3conf/ext/helfen_kann_jeder/Tests/Build/FunctionalTests.xml {}'
 fi
+
+if [[ "$COVERAGE" == "1" ]]; then
+	phpcov merge --clover typo3conf/ext/helfen_kann_jeder/clover.xml $TEMPFOLDER
+fi
+rm -rf $TEMPFOLDER
