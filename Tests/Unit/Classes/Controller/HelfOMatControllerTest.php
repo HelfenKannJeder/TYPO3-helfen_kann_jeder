af05e32889e3b73e198154292d2cb57203e0fb60
Valentin Zickner <zickner@querformatik.de>
2015-03-11 23:03:46 +0100

    Improved unit tests for helf-o-mat.

--- Tests/Unit/Classes/Controller/HelfOMatControllerTest.php
+++ Tests/Unit/Classes/Controller/HelfOMatControllerTest.php
@@ -33,6 +33,16 @@ class HelfOMatControllerTest  extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 	protected $helfomatRepository;
 
 	/**
+	 *
+	 */
+	protected $cookieService;
+
+	/**
+	 *
+	 */
+	protected $searchService;
+
+	/**
 	 * @return void
 	 */
 	public function setUp() {
@@ -56,14 +66,28 @@ class HelfOMatControllerTest  extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 
 		$this->settings = array(
 			'helfomat' => array(
-				'default' => 42
-			)
+				'default' => 42,
+			),
+			'config' => array(
+				'maxDistance' => 100,
+			),
+			'page' => array(
+				'overview' => array(
+					'detail' => 1337,
+				),
+			),
 		);
 		$this->controller->_set('settings', $this->settings);
 
 		$this->helfomatRepository = $this->getMock('\Querformatik\HelfenKannJeder\Domain\Model\HelfOMat', array('findByUid'));
-
 		$this->inject($this->controller, 'helfomatRepository', $this->helfomatRepository);
+
+		$this->cookieService = $this->getMock('\Querformatik\HelfenKannJeder\Service\CookieService', array('getPersonalCookie'));
+		$this->inject($this->controller, 'cookieService', $this->cookieService);
+
+		$this->searchService = $this->getMock('\Querformatik\HelfenKannJeder\Service\OrganisationSearchService',
+			array('findOrganisations'));
+		$this->inject($this->controller, 'searchService', $this->searchService);
 	}
 
 	/**
@@ -104,5 +128,107 @@ class HelfOMatControllerTest  extends \TYPO3\CMS\Core\Tests\UnitTestCase {
 				$this->identicalTo($helfomat)
 			);
 		$this->controller->quizAction($helfomat);
+	}
+
+	/**
+	 * @test
+	 * @return void
+	 */
+	public function testGroupResultAction() {
+		$helfomat = new HelfOMat();
+		$answer = array(
+			10 => 1,
+			11 => 2,
+			12 => 0,
+			9 => 2,
+			15 => 0
+		);
+		$organisations = array(
+			10 => array(
+				'something special'
+			)
+		);
+		$this->view
+			->expects($this->at(0))
+			->method('assign')
+			->with(
+				$this->identicalTo('answer'),
+				$this->identicalTo($answer)
+			);
+		$this->view
+			->expects($this->at(1))
+			->method('assign')
+			->with(
+				$this->identicalTo('helfOMat'),
+				$this->identicalTo($helfomat)
+			);
+		$this->view
+			->expects($this->at(2))
+			->method('assign')
+			->with(
+				$this->identicalTo('normGradeToMax'),
+				$this->identicalTo(1 / 100)
+			);
+		$this->view
+			->expects($this->at(3))
+			->method('assign')
+			->with(
+				$this->identicalTo('organisations'),
+				$this->identicalTo($organisations)
+			);
+
+		$this->pTestCalculateGroupResult($answer, $organisations);
+
+		$this->controller->groupResultAction($helfomat, $answer);
+	}
+
+	/**
+	 * @test
+	 * @return void
+	 */
+	public function testJsonGroupResultAction() {
+		$answer = array(
+			10 => 1,
+			11 => 2,
+			12 => 0,
+			9 => 2,
+			15 => 0
+		);
+		$organisations = array(
+			10 => array(
+				'something special'
+			)
+		);
+		$this->pTestCalculateGroupResult($answer, $organisations);
+
+		$result = $this->controller->jsonGroupResultAction($answer);
+		$this->assertEquals('{"10":["something special"]}', $result);
+	}
+
+	/**
+	 * @test
+	 * @return void
+	 */
+	public function testBuildOrganisationUri() {
+		// TODO: Build this test
+		$this->assertTrue(TRUE);
+	}
+
+	/**
+	 * @return void
+	 */
+	private function pTestCalculateGroupResult($answer, $organisations) {
+		$this->cookieService
+			->expects($this->any())
+			->method('getPersonalCookie')
+			->will($this->returnValue(array(49.1234, 8.2313, 18)));
+
+		$this->searchService
+			->expects($this->once())
+			->method('findOrganisations')
+			->with(
+				49.1234, 8.2313, $answer, 100, array(&$this->controller, 'buildOrganisationUri')
+			)
+			->will($this->returnValue($organisations));
 	}
 }
